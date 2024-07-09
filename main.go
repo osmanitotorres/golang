0e0e95d9fdaf81413714159f418b60680fc8dcb3
Osmanito Torres <46090429+osmanitotorres@users.noreply.github.com>
2024-07-09 09:49:47 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	_ "fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("<b>It's Work!!!</b>")
+	})
+
+	app.Listen(":3000")
 }
